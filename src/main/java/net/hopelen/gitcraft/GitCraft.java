d3bd7c30a622beae5c48fd21fe14369b44f5a775
Hope <roni.trololo2004@gmail.com>
2026-05-07 20:05:36 +0300

    Initial commit

---
+++ src/main/java/net/hopelen/gitcraft/GitCraft.java
@@ -0,0 +1,21 @@
+package net.hopelen.gitcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GitCraft implements ModInitializer {
+    public static final String MOD_ID = "gitcraft";
+
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+        LOGGER.info("Hello from GitCraft");
+    }
+}
---
+++ src/main/java/net/hopelen/gitcraft/GitCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hopelen.gitcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GitCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
